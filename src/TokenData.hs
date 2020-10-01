5a1248ef5cb9b249dd3ffff8ba19b772d3ef6b40
Dylan Turner <dylantdmt@gmail.com>
2020-10-01 10:57:02 -0400

    Implemented token data types

--- src/TokenData.hs
+++ src/TokenData.hs
@@ -1,2 +1,17 @@
-module TokenData where
+module TokenData(SymbolTokenType, CompoundTokenType, Token) where
 
+data SymbolTokenType =  Keyword         | Boolean       | IntType
+                        | FloatingPoint | Character     | StringLiteral
+                        | TypeChar      | Identifier    | Parenth
+                        | Bracket       | Brace         | ReturnOp
+                        | DoubleArrow   | Operator      | TypeOp
+                        | MemberOp
+
+data CompoundTokenType =    RawType         | Tuple         | List
+                            | StructAccess  | Type          | TypeName
+                            | Body          | FuncDef       | Loop
+                            | Cast          | StructDef     | Stmt
+                            | Program
+
+data Token =    SymbolToken SymbolTokenType String (Int, Int)
+                | CompoundToken CompoundTokenType [ Token ]
