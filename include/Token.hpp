ec4afc93688871e952cce0d16d0c793f66df615c
Dylan Turner <dylantdmt@gmail.com>
2020-10-05 01:21:48 -0400

    Defined token types

--- include/Token.hpp
+++ include/Token.hpp
@@ -1,5 +1,62 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <utility>
+
+/*
+ * These are more elegant in haskell
+ * Consider:
+ * 
+ * data SymbolTokenType = Keyword | Boolean | IntType | Float | ... | NoSymbol
+ * data CompoundTokenType = RawType | Tuple | List | ... | NoCompound
+ * data Token =
+ *      SymbolToken SymbolTokenType String (Int, Int)
+ *      | CompoundToken CompoundTokenType [Token]
+ * 
+ * I've chosen to use Inheritance here because this shouldn't really change,
+ * it matches the UML, and it works
+ */ 
 namespace kisslang {
     
+    struct Token {};
+    
+    enum class SymbolTokenType {
+        Keyword = 'k',      Boolean = 'b',      Integer = 'i',
+        Float = 'f',        Character = 'c',    String = '\'',
+        TypeChar = '@',     Identifier = 'n',   Parenth = '(', 
+        Bracket = '[',      Brace = '{',        ReturnOp = '>',
+        DoubleArrow = '<',  Operator = '=',     TypeOp = ':',
+        MemberOp = '.',
+        
+        None = '?'
+    };
+    
+    struct SymbolToken : public Token {
+        const SymbolTokenType type;
+        const std::string value;
+        const std::pair<int, int> position;
+        
+        SymbolToken(
+            SymbolTokenType type, std::string value,
+            std::pair<int, int> position
+        );
+    };
+    
+    enum class CompoundTokenType {
+        RawType = 'r',      Tuple = ',',        List = 'l',
+        Struct = 's',       StructAccess = 'S', Type = 't',
+        TypeName = 'N',     Body = '}',         FuncDef = 'F',
+        Loop = 'L',         Cast = 'a',         StructDef = 'd',
+        Statement = 'I',    Program = 'p',
+        
+        None = '!'
+    };
+    
+    struct CompoundToken : public Token {
+        const CompoundTokenType type;
+        const std::vector<Token> children;
+        
+        CompoundToken(CompoundTokenType type, std::vector<Token> children);
+    };
 }
--- src/Token.cpp
+++ src/Token.cpp
@@ -1,3 +1,17 @@
+#include <string>
+#include <vector>
+#include <utility>
 #include <Token.hpp>
 
 using namespace kisslang;
+
+SymbolToken::SymbolToken(
+        SymbolTokenType type, std::string value,
+        std::pair<int, int> position) :
+        type(type), value(value), position(position) {
+}
+
+CompoundToken::CompoundToken(
+        CompoundTokenType type, std::vector<Token> children) :
+        type(type), children(children) {
+}
