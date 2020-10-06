1cc9a7010b5f8613b6162c692291eacc00bbdd65
Dylan Turner <dylantdmt@gmail.com>
2020-10-06 01:21:28 -0400

    Moved try catch out of inline function to simplify primary path of compilation

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,15 @@
 
 using namespace kisslang;
 
-inline const int compileSourceCode(
+inline void compileSourceCode(
         const std::string &source, const std::string &moduleName) {
-    std::cout << "Compiling module " << moduleName << "." << std::endl;
-    try {
-        const auto tokens = Parser::lexTokens(source);
-        const auto ast = Parser::parseAst(tokens);
-        std::cout << ast.str() << std::endl;
-        const auto cppCode = Compiler::generateCode(ast);
-        Compiler::compile(moduleName, cppCode);
-    } catch(const UnknownTokenException &ute) {
-        std::cout << ute.what() << std::endl;
-        return -1;
-    } catch(const UnexpectedTokenException &ute) {
-        std::cout << ute.what() << std::endl;
-        return -1;
-    }
-    return 0;
+    const auto tokens = Parser::lexTokens(source);
+    
+    const auto ast = Parser::parseAst(tokens);
+    std::cout << ast.str() << std::endl;
+    
+    const auto cppCode = Compiler::generateCode(ast);
+    Compiler::compile(moduleName, cppCode);
 }
 
 int main(int argc, char **args) {
@@ -32,5 +24,15 @@ int main(int argc, char **args) {
     if(Io::sourceCodeFromArgs(sourceCode, moduleName, argc, args)) {
         return -1;
     }
-    return compileSourceCode(sourceCode, moduleName);
+    std::cout << "Compiling module " << moduleName << "." << std::endl;
+    try {
+        compileSourceCode(sourceCode, moduleName);
+    } catch(const UnknownTokenException &ute) {
+        std::cout << ute.what() << std::endl;
+        return -1;
+    } catch(const UnexpectedTokenException &ute) {
+        std::cout << ute.what() << std::endl;
+        return -1;
+    }
+    return 0;
 }
